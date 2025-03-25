2cf9be6ead30f146b011eaef786ed77afcb8aa48
SreemahaSrinivasan <srimahas2001@gmail.com>
2025-03-25 16:41:38 +0000

    code updated

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,1 +1,15 @@
 // Write your code here
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {name, imgUrl} = destinationDetails
+  return (
+    <li className="destination-item">
+      <img src={imgUrl} className="destination-image" alt={name} />
+      <p className="name">{name}</p>
+    </li>
+  )
+}
+
+export default DestinationItem
--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,1 +1,50 @@
-// Write your code here
+import './index.css'
+import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
+
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {destinationsList} = this.props
+    const {searchInput} = this.state
+    const searchedResults = destinationsList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div className="app-container">
+        <div className="destination-search-container">
+          <h1 className="heading">Destination Search</h1>
+          <div className="search-input-container">
+            <input
+              className="search-input"
+              type="search"
+              placeholder="Search"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              alt="search icon"
+              className="search-icon"
+            />
+          </div>
+          <ul className="destinations-list">
+            {searchedResults.map(eachDestination => (
+              <DestinationItem
+                destinationDetails={eachDestination}
+                key={eachDestination.id}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
